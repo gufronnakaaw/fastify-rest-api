3446bd2593dd22f8067cd3a4240262d26e1bae3c
gufronnakaaw <gufronnakaaw@gmail.com>
2023-07-18 10:16:52 +0700

    add: check connection to database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config';
 import build from '../utils/server';
+import prisma from '../utils/database';
+import logger from '../utils/logger';
 
 const port: number = Number(process.env.port);
 
@@ -7,8 +9,9 @@ async function app() {
   const server = build();
 
   try {
+    await prisma.$connect();
     await server.listen({ port });
-    console.log(`server is running at http://localhost:${port}`);
+    logger.info(`server is running at http://localhost:${port}`);
   } catch (error) {
     server.log.error(error);
     process.exit(1);
