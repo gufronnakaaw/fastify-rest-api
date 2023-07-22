3cc75fcf897f1bd01403ba05aabf443ff9962a60
gufronnakaaw <gufronnakaaw@gmail.com>
2023-07-22 21:46:37 +0700

    delete: unique constraint

---
+++ prisma/migrations/20230722144445_delete_unique_constraint/migration.sql
@@ -0,0 +1,17 @@
+-- DropIndex
+DROP INDEX `products_name_key` ON `products`;
+
+-- DropIndex
+DROP INDEX `products_slug_key` ON `products`;
+
+-- DropIndex
+DROP INDEX `sellers_domain_key` ON `sellers`;
+
+-- DropIndex
+DROP INDEX `sellers_email_key` ON `sellers`;
+
+-- DropIndex
+DROP INDEX `sellers_name_key` ON `sellers`;
+
+-- DropIndex
+DROP INDEX `sellers_phone_key` ON `sellers`;
